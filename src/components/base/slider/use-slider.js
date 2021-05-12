1783afbc9b06a51f3a26626d90fe5fedb6765a9c
Max Cheng <ofcoursemax@gmail.com>
2021-05-12 12:31:54 +0800

    :sparkles: feat: add recommend pic slide banner

---
+++ src/components/base/slider/use-slider.js
@@ -0,0 +1,36 @@
+import BScroll from '@better-scroll/core'
+import Slide from '@better-scroll/slide'
+
+import { onMounted, onUnmounted, ref } from 'vue'
+
+BScroll.use(Slide)
+
+export default function useSlider (wrapperRef) {
+  const currentPageIndex = ref(0)
+  const slider = ref(null)
+
+  onMounted(() => {
+    const sliderVal = slider.value = new BScroll(wrapperRef.value, {
+      click: true,
+      scrollX: true,
+      scrollY: false,
+      momentum: false,
+      bounce: false,
+      probeType: 2,
+      slide: true
+    })
+
+    sliderVal.on('slideWillChange', (page) => {
+      currentPageIndex.value = page.pageX
+    })
+  })
+
+  onUnmounted(() => {
+    slider.value.destroy()
+  })
+
+  return {
+    slider,
+    currentPageIndex
+  }
+}
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,22 +1,78 @@
 <template>
   <div class="recommend">
-    推荐
+<!--    <div class="slider">-->
+<!--      <div class="slider__wrapper">-->
+<!--        <div class="slider__wrapper__content">-->
+<!--          <slider v-if="sliders.length" :sliders="sliders"></slider>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+    <div class="slider-wrapper">
+      <div class="slider-content">
+        <slider v-if="sliders.length" :sliders="sliders"></slider>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
 
 export default {
   name: 'recommend',
+  data () {
+    return {
+      sliders: []
+    }
+  },
+  components: {
+    Slider
+  },
   // 创建生命周期时获取轮播图
   async created () {
     const result = await getRecommend()
     console.log(result)
+    this.sliders = result.sliders
   }
 }
 </script>
 
-<style lang="sass" scoped>
-
+<style lang="scss" scoped>
+.recommend{
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+  .slider{
+    &__wrapper{
+      position: relative;
+      width: 100%;
+      height: 0;
+      padding-top: 40%;
+      overflow: hidden;
+      &__content {
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+  .slider-wrapper {
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding-top: 40%;
+    overflow: hidden;
+    .slider-content {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
 </style>
