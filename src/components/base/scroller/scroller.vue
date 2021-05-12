950f04fd95b253e6eb640a05040e7f2b27741821
Max Cheng <ofcoursemax@gmail.com>
2021-05-12 19:16:58 +0800

    :sparkles: feat: add recommend list

---
+++ src/components/base/scroller/scroller.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="rootRef">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import useScroller from '@/components/base/scroller/use-scroller'
+import { ref } from 'vue'
+
+export default {
+  name: 'scroller',
+  props: {
+    click: {
+      type: Boolean,
+      default: true
+    }
+  },
+  setup (props) {
+    const rootRef = ref(null)
+    useScroller(rootRef, props)
+
+    return { rootRef }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
